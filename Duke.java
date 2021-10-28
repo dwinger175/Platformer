3fe889515b18dc7605dbe580070883cfc9f8d30f
Dylan Winger <20423@my4county.net>
2021-10-28 11:35:33 -0400

    Start of jump game

---
+++ Duke.java
@@ -0,0 +1,213 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Duke here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Duke extends Actor
+{
+    private int speed = 7;
+    private int vSpeed = 0;
+    private double accel = 1;
+    private int jumpStrength = 16;
+    private int jtime = 0;
+    private boolean jumping;
+    private int direction;
+
+    private GreenfootImage player1r = new GreenfootImage("catProtagR.png");
+    private GreenfootImage player1l = new GreenfootImage("catProtag.png");
+    private GreenfootImage player1JumpR = new GreenfootImage("catdoJumpR.png");
+    private GreenfootImage player1JumpL = new GreenfootImage("catdoJumpL.png");
+
+    
+    /**
+     * Act - do whatever the Duke wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        checkKeys();
+        fall();
+        checkFall();
+        platformAbove();
+    }
+
+    public void checkKeys()
+    {
+        if (Greenfoot.isKeyDown("right"))
+        {
+            moveRight();
+            setImage(player1r);
+            direction = 1;
+        }
+
+        if (Greenfoot.isKeyDown("left"))
+        {
+            moveLeft();
+            setImage(player1l);
+            direction = 2;
+        } 
+
+        if (Greenfoot.isKeyDown("right") && jumping == true)
+        {
+  
+            setImage(player1JumpR);
+            direction = 1;
+        } 
+
+        if (Greenfoot.isKeyDown("left") && jumping == true)
+        {
+
+            setImage(player1JumpL);
+            direction = 2;
+        } 
+
+        
+        if (Greenfoot.isKeyDown("space") && jumping == false)
+        {
+            jump();
+
+        } 
+    }
+    public void moveRight()
+    {
+        setLocation (getX() + speed, getY());
+    }
+
+    public void moveLeft()
+    {
+        setLocation (getX() - speed, getY());
+    }
+
+    public void fall()
+    {
+        setLocation (getX(), getY() + vSpeed);
+        jumping = true;
+     
+        if (vSpeed <=9)
+        {
+            vSpeed = vSpeed += accel;
+        }
+
+    }
+
+    public boolean onGround()
+    {
+        int spriteHeight = getImage().getHeight();
+        int yDistance = (int)(spriteHeight/2) + 5;
+        Actor ground = getOneObjectAtOffset(0, getImage().getHeight() / 2 , Platform.class);
+
+        if (ground == null)
+        {
+            jumping = true;
+            return false;
+        }
+        else
+        {
+            moveToGround(ground);
+            return true;
+        }
+
+        
+
+    }
+
+    public void moveToGround (Actor ground)
+    {
+        int groundHeight = ground.getImage().getHeight();
+        int newY = ground.getY() - (groundHeight + getImage().getHeight())/2;
+        setLocation(getX(), newY);
+        jumping = false;
+
+        if (direction == 1)
+        {
+            setImage(player1r);
+
+        }
+
+        if (direction == 2) {
+            setImage(player1l);
+
+        }
+        
+    }
+
+    
+    
+    public void checkFall()
+    {
+        if(onGround())
+        {
+            vSpeed = 0;
+
+        }
+        else
+        {
+            fall();
+
+        }
+    }
+
+    public void jump()
+    {
+        if (jtime == 3 )
+        {
+            vSpeed = -jumpStrength;
+            fall();
+            jtime = 0;
+        }
+        else
+        {
+
+            jtime++;
+        }
+
+    }
+
+    /**
+     * 
+     * Checks if there is a platform above you
+     *  Code basics from awesomeinmyworld on greenfoot
+     * 
+     */
+    public boolean platformAbove()
+    {
+        int spriteHeight = getImage().getHeight();
+        int yDistance = (int)(spriteHeight/-2);
+        Actor ceiling = getOneObjectAtOffset(0,yDistance, Platform.class);
+        if (ceiling != null)
+        {
+            vSpeed = 1;
+            bopHead(ceiling);
+            return true;
+
+        }
+        else
+        {
+            return false; 
+
+        }
+
+    }
+
+    /**
+     * Stops jump if there is a platform above you.
+     * 
+     *  Code basics from awesomeinmyworld on greenfoot
+     * 
+     */    
+    public void bopHead(Actor ceiling)
+    {
+        int ceilingHeight = ceiling.getImage().getHeight();
+        int newY = ceiling.getY() + (ceilingHeight + getImage().getHeight())/2;
+        setLocation(getX(), newY);
+    }
+
+
+
+
+
+
+}
---
+++ MyWorld.java
@@ -0,0 +1,38 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(800, 600, 1); 
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+
+        Platform platform = new Platform();
+        addObject(platform,17,575);
+        Platform platform2 = new Platform();
+        addObject(platform2,614,575);
+        Duke duke = new Duke();
+        addObject(duke,724,511);
+        duke.setLocation(30,525);
+    }
+}
